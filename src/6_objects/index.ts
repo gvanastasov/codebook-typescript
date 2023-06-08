e4db2fb72e5e4a3853ed3c91406895befa027318
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-08 21:18:02 +0200

    add optional properties section

--- src/6_objects/index.ts
+++ src/6_objects/index.ts
@@ -1,5 +1,24 @@
 (
     /**
+     * @description the question mark after a property name, 
+     * you specify that the property is optional, meaning it 
+     * may or may not be present in object
+     */
+    function optionalProperty() {
+        try {
+            // @ts-expect-error
+            let object_err: { x: number, y: number } = { x: 3 }
+        } catch {
+            console.log("Property 'y' is missing in type '{ x: number; }' but required in type '{ x: number; y: number; }'");
+        }
+
+        // OK
+        let object: { x: number, y?: number } = { x: 3 }
+    }
+)();
+
+(
+    /**
      * @description object nesting can be defined with any valid type definition
      * for instance inline, or with interface, or with alias.
      * 
