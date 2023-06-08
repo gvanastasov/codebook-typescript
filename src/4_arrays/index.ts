9107dcc584ee2be78d21cc598bca8b67c1bcbe7b
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-08 17:13:08 +0200

    update chapter

--- src/4_arrays/index.ts
+++ src/4_arrays/index.ts
@@ -1,5 +1,21 @@
-(function() {
-    let digits: number[] = [0,1,2,3,4,5,6,7,8,9];
+(
+    /**
+     * @description Arrays in TypeScript provide a structured and 
+     * type-safe way to work with collections of data. They offer 
+     * a range of methods and operations to manipulate array elements 
+     * effectively. Reminder - TypeScript supersets Javascript, therefore
+     * the behavior of array is retained as is. Out-of-range error is not
+     * handled but rather returns value of undefined, which is not considered
+     * an error.
+     */
+    function syntax() {
+        let digits: number[] = [0,1,2,3,4,5,6,7,8,9];
+        let weekdays: Array<String> = ['Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    let digits_1: Array<number> = [...digits];
-})()
+        // OK
+        console.log(digits[1]);
+
+        // OK
+        console.log(weekdays[8]);
+    }
+)();
