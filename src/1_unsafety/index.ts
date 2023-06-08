279fd8d37a5592bb97c37a26a78b78f79f3f586d
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-08 15:26:59 +0200

    add hidden unsafety section

--- src/1_unsafety/index.ts
+++ src/1_unsafety/index.ts
@@ -20,3 +20,29 @@
         message.toLowerCase();
     }
 )();
+
+(
+    /**
+     * @description no error during 'development', BUT error during 'run' 
+     * even with type safety checks enabled for javascript/ts simply because 
+     * type cannot be inferred by the invocation and the logic inside the 
+     * method is hidden, accepting 'any' type of object.
+     */
+    
+    function hidden() {
+        function fn(x: any): void {
+            x.callback();
+        }
+        
+        const message = "Dummy message.";
+
+        try {
+            fn(message);
+        } catch {
+            console.log("Property 'callback' does not exist on type 'string'");
+        }
+
+        // OK
+        fn({ callback: () => {}});
+    }
+)();
