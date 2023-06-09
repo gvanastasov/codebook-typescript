a6af1d436583b33ce6f3e1952a772734ce91aa05
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-10 00:19:24 +0200

    add class chapter

---
+++ src/13_class/index.ts
@@ -0,0 +1,11 @@
+(
+    /**
+     * @description provide a way to define blueprints for creating 
+     * objects with shared properties and behaviors. They are a 
+     * fundamental part of object-oriented programming and allow you 
+     * to create reusable and organized code.
+     */
+    function syntax() {
+        class User {};
+    }
+)();
