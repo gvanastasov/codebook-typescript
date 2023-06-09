9e4ea329e1501840d533932d89de2561634acd01
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-09 11:23:25 +0200

    add contract section

--- src/8_interfaces/index.ts
+++ src/8_interfaces/index.ts
@@ -1,8 +1,19 @@
 (
     /**
-     * @description sed to define the structure and shape of 
-     * objects. They provide a way to specify the contract 
-     * that an object must adhere to.
+     * @description core difference between types and interfaces in 
+     * TypeScript lies in their nature and behavior. While both types 
+     * and interfaces can be used to define custom types, they have 
+     * some distinctions:
+     *      - declaration - interface vs. type
+     *      - extensibility - interfaces can be extended (see below), 
+     *        while types cannot be.
+     *      - interchangeability 
+     *      - features - Types have some features that interfaces do 
+     *        not have, such as the ability to define mapped types, 
+     *        conditional types, and utility types like Partial, Pick, 
+     *        and Record. These features make types more flexible and 
+     *        powerful in certain scenarios, especially when working 
+     *        with complex type transformations and manipulations.
      */
     function syntax() { 
         interface IPoint3D {
@@ -13,6 +24,29 @@
         let p: IPoint3D = { x: 5, y: 5 };
 
         console.log(`Coordinates: ${p.x}, ${p.y}`);
+    }
+)();
+
+(
+    /**
+     * @description sed to define the structure and shape of 
+     * objects. They provide a way to specify the contract 
+     * that an object must adhere to.
+     */
+    function contract() {
+        interface IAnimal {
+            readonly sound: string; 
+        };
+
+        function makeSound(animal: IAnimal) {
+            console.log(`${animal.sound}`);
+        }
+
+        let dog: IAnimal = { sound: "bark" };
+        let cat: IAnimal = { sound: "meoy" };
+
+        makeSound(dog);
+        makeSound(cat);
     }
 )();
 
